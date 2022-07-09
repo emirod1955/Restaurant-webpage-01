e583cc0e6a6bf98a92eb11bd2c01f703e384b125
emirod <emirod@penguin>
2022-07-08 22:59:57 -0300

    Rediseño de la pagina del local en proceso, falta parte del responsive

--- src/components/Local/Local.jsx
+++ src/components/Local/Local.jsx
@@ -4,24 +4,31 @@ import React from "react";
 //import css
 import './Local.css';
 
+//import img
+import foto1 from './img/foto1.jpeg';
+import foto2 from './img/foto2.jpg';
+import foto3 from './img/foto3.jpg';
+import foto4 from './img/foto4.jpg';
+import foto5 from './img/foto5.jpg';
+import foto6 from './img/foto6.jpg';
+
+
 function Local(){
     return(
-        <div  className="AllLocal">
-            <div className="DivTitulo"><h1 className="LocalTitle">Fotos de nuestro restaurante</h1></div>            
+        <div className="AllLocal">
+            <div className="DivTitulo"><h1 className="LocalTitle">Nuestro restoran</h1></div>            
             <div className="ImageBlockContainer">
-                <div className="ImageContainer">
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
-                    <img id="foto" src="https://i.ibb.co/3vxg7m5/ejemplo.jpg" alt="" />
+                <div className="fila-flex-1">
+                    <img src={foto1} alt="" />
+                </div>
+                <div className="fila-flex-2">
+                    <img id="foto2" src={foto2} alt="" />
+                    <img id="foto3" src={foto3} alt="" />
+                </div>
+                <div className="fila-flex-3">
+                    <img id="foto4" src={foto4} alt="" />
+                    <img id="foto5" src={foto5} alt="" />
+                    <img id="foto6" src={foto6} alt="" />
                 </div>
             </div>
         </div>
